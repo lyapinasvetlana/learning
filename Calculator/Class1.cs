f1ccf6721eba1ee76f8b3585231dae6d02e5ce6c
Svetlana Lyapina <lyapinasveta1990@gmail.com>
2020-10-11 01:47:55 +0300

    добавили калькулятор

---
+++ Calculator/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+       public static Sum(int a, int b){ 
+         return a+b;
+       }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   int x=10;
+            int y=x+2;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
